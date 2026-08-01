41ed7cd4ad9ce72ad9e461c834712d147c064968
sandipbeldare <sandipbeldare17@gmail.com>
2026-08-01 21:19:18 +0530

    Delete Launch Sandip Office System.cmd

--- Launch Sandip Office System.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "Sandip Office Server" /min cmd /c "node --experimental-sqlite server.mjs"
-timeout /t 2 /nobreak >nul
-start "Sandip Beldare Office System" "http://localhost:4173"
